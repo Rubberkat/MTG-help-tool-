c57b4cb3c324545a83f2770effaecf476372a932
Rubberkat <roydejager@live.nl>
2015-11-12 23:32:40 +0100

    Update and rename sql_functions.py to life_functions.py

---
+++ life_functions.py
@@ -0,0 +1,61 @@
+__author__ = 'Roy'
+import sqlite3
+
+conn =  sqlite3.connect('life.db')
+cur = conn.cursor()
+
+create_db = "CREATE TABLE IF NOT EXISTS lifetotal" \
+            "(ID INTEGER PRIMARY KEY AUTOINCREMENT, " \
+            "life INTEGER DEFAULT 20);"
+
+create_life = "INSERT INTO lifetotal DEFAULT VALUES"
+
+cur.execute(create_db)
+cur.execute(create_life)
+conn.commit()
+
+
+class sql_functions:
+
+    sql = "SELECT life FROM lifetotal;"
+    cur.execute(sql)
+    results = cur.fetchall()
+    life_list = int(results[0][0])
+
+    def __init__(self, x):
+        self.starting_life = x
+
+
+
+
+
+def gainLife():
+    gain = sql_functions
+    gain_life = int(input('Enter a number: '))
+    gain_total = gain.life_list + gain_life
+    cur.execute("UPDATE lifetotal "
+                "SET life = ? WHERE ID = 1",
+                (gain_total,))
+    conn.commit()
+
+
+def loseLife():
+    lose = sql_functions
+    lose_life = int(input ('Enter a number: '))
+    lost_total = lose.life_list - lose_life
+    cur.execute("UPDATE lifetotal "
+                "SET life = ? WHERE ID = 1",
+                (lost_total,))
+    conn.commit()
+
+
+def resetLifeTotal():
+    reset = sql_functions(20)
+    cur.execute("UPDATE lifetotal "
+                "SET life = ? WHERE ID = 1",
+                (reset.starting_life,))
+    conn.commit()
+
+
+
+
