2d5d9138037d4fff1cefac4ad49e4efce6c75492
Rubberkat <roydejager@live.nl>
2016-01-31 03:24:24 +0100

    Update life_functions.py

--- life_functions.py
+++ life_functions.py
@@ -1,70 +1,59 @@
 __author__ = 'Roy'
 import sqlite3
 
+
 conn =  sqlite3.connect('life.db')
 cur = conn.cursor()
 
-create_db = "CREATE TABLE IF NOT EXISTS lifetotal" \
-            "(ID INTEGER PRIMARY KEY AUTOINCREMENT, " \
-            "life INTEGER DEFAULT 20);"
+cur.execute ("CREATE TABLE IF NOT EXISTS lifetotal"
+            "(ID INTEGER PRIMARY KEY AUTOINCREMENT, life INTEGER DEFAULT 20);")
 
-create_life = "INSERT INTO lifetotal DEFAULT VALUES"
+select = "SELECT ID FROM lifetotal WHERE ID = 1"
+cur.execute(select)
+results = cur.fetchone()
+if not results:
+    cur.execute ("INSERT INTO lifetotal DEFAULT VALUES")
 
-cur.execute(create_db)
-cur.execute(create_life)
 conn.commit()
 
 
-class starting_life_list:
-
-    sql = "SELECT life FROM lifetotal;"
-    cur.execute(sql)
-    results = cur.fetchall()
-    life_list = int(results[0][0])
-
-    def __init__(self, x):
-        self.starting_life = x
+cur.execute("SELECT life FROM lifetotal;")
+results = cur.fetchall()
+life_list = results[0][0]
 
 
+class player:
+
+    ID = 0
+    starting_life = 20
 
 
+    def gainLife(self):
 
-def gainLife():
-    gain = starting_life_list
-    gain_life = int(input('Enter a number: '))
-    gain_total = gain.life_list + gain_life
-    
-    cur.execute(
-                "UPDATE lifetotal "
-                "SET life = ? WHERE ID = 1",
-                (gain_total,)
-    )
-    conn.commit()
+        try:
+            gain_life = int(input('Enter a number: '))
+            gain_total = life_list + gain_life
+            cur.execute("UPDATE lifetotal SET life = ? WHERE ID = ?",
+                        (gain_total, self.ID,))
+            conn.commit()
+        except:
+            print ('Error, please enter a number.')
 
 
-def loseLife():
-    lose = starting_life_list
-    lose_life = int(input ('Enter a number: '))
-    lost_total = lose.life_list - lose_life
-    
-    cur.execute(
-                "UPDATE lifetotal "
-                "SET life = ? WHERE ID = 1",
-                (lost_total,)
-    )
-    conn.commit()
+    def loseLife(self):
+
+        try:
+            lose_life = int(input ('Enter a number: '))
+            lost_total = life_list - lose_life
+            cur.execute("UPDATE lifetotal SET life = ? WHERE ID = ?",
+                        (lost_total, self.ID,))
+            conn.commit()
+        except:
+            print ('Error, please enter a number.')
 
 
-def resetLifeTotal():
-    reset = starting_life_list(20)
-    
-    cur.execute(
-                "UPDATE lifetotal "
-                "SET life = ? WHERE ID = 1",
-                (reset.starting_life,)
-    )
-    conn.commit()
-
-
-
-
+    def resetLifeTotal(self):
+        print ('Life has been reset to:', self.starting_life)
+        cur.execute("UPDATE lifetotal SET life = ? WHERE ID = ?",
+                    (self.starting_life, self.ID,))
+        conn.commit()
